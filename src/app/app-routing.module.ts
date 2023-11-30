fc5935a02a9a913294a6eb366aefd1089a0a568e
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2023-11-30 17:09:23 +0100

    maj

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,11 @@ const routes: Routes = [
   {path:"front",loadChildren:()=>
   import('./front-office/front-office.module').then(m=>(m).FrontOfficeModule)},
   {path:"back",loadChildren:()=>
+ 
   import('./back-office/back-office.module').then(m=>(m).BackOfficeModule)},
   {path:"user",loadChildren:()=>
   import('./user/user.module').then(m=>m.UserModule)},
+
   {path: '', redirectTo: '**', pathMatch: 'full' },
   {path:"**", component:NotFoundComponent}
 ];
