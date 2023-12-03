4fdd03a57bdb4cb0ecfb4d7be3d33754abdd6b0c
MouhamedAbbassi <mouhamed.abbassi@esprit.tn>
2023-12-03 03:33:16 +0100

    merge bloc

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -32,7 +32,7 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
     BrowserAnimationsModule,
     FrontOfficeModule,
     BackOfficeModule,
- 
+
 
     HttpClientModule,
     UserModule,
